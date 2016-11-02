b324babc310c7b4db3346942ee9a8ee0f1e7efe1
ystvan <istvan.marki@gmail.com>
2016-11-02 20:58:48 +0100

    interface using HTTP
    exposing services for CRUD operations

--- MyFirstRESTfulAPI/IBookService.cs
+++ MyFirstRESTfulAPI/IBookService.cs
@@ -12,8 +12,28 @@ namespace MyFirstRESTfulAPI
     [ServiceContract]
     public interface IBookService
     {
+        [OperationContract]
+        [WebInvoke(Method = "GET", RequestFormat = WebMessageFormat.Json, ResponseFormat = WebMessageFormat.Xml, UriTemplate = "Books/")]
+        List<Book> GetBookList();
 
-        
+        [OperationContract]
+        [WebInvoke(Method = "GET", RequestFormat = WebMessageFormat.Json, ResponseFormat = WebMessageFormat.Json, UriTemplate = "Book/{id}")]
+        Book GetBookById(string id);
+
+        [OperationContract]
+        [WebInvoke(Method = "PUT", RequestFormat = WebMessageFormat.Json, ResponseFormat = WebMessageFormat.Json, UriTemplate = "AddBook/{id}")]
+        string AddBook(Book book, string id);
+
+        [OperationContract]
+        [WebInvoke(Method = "PUT", RequestFormat = WebMessageFormat.Json, ResponseFormat = WebMessageFormat.Json, UriTemplate = "UpdateBook/{id}")]
+        string UpdateBook(Book book, string id);
+
+        [OperationContract]
+        [WebInvoke(Method = "DELETE", RequestFormat = WebMessageFormat.Json, ResponseFormat = WebMessageFormat.Json, UriTemplate = "DeleteBook/{id}")]
+        string DeleteBook(string id);
+
+
+
     }
 
     
