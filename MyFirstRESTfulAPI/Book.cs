3f605313dc8f65912c02309d60a3ab52642beb93
ystvan <istvan.marki@gmail.com>
2016-11-02 20:09:40 +0100

    Add project files.

---
+++ MyFirstRESTfulAPI/Book.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Web;
+
+namespace MyFirstRESTfulAPI
+{
+    [DataContract]
+    public class Book
+    {
+        [DataMember]
+        public int BookId { get; set; }
+
+        [DataMember]
+        public string Title { get; set; }
+
+        [DataMember]
+        public string ISBN { get; set; }
+
+    }
+}
