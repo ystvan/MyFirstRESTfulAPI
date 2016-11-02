3ad3aab007aae2b99cafa54a812c655a16563b64
ystvan <istvan.marki@gmail.com>
2016-11-02 20:44:15 +0100

    BookRepository
    implementing interface pt.2

--- MyFirstRESTfulAPI/Book.cs
+++ MyFirstRESTfulAPI/Book.cs
@@ -32,29 +32,112 @@ namespace MyFirstRESTfulAPI
 
     public class BookRepository : IBookRepository
     {
-        public Book AddNewBook(Book item)
+        private List<Book> books = new List<Book>();
+        private int counter = 1;
+
+        public BookRepository()
         {
-            throw new NotImplementedException();
+            AddNewBook(new Book() {Title = "Ungdomskort i alle zoner", ISBN = "061116-051216"});
+            AddNewBook(new Book() { Title = "Rage Against the answering Machine", ISBN = "161205-161106" });
+            AddNewBook(new Book() { Title = "Blue Bird Cafe", ISBN = "589302-986987" });
+            AddNewBook(new Book() { Title = "Pap og Carton", ISBN = "859632-025145" });
         }
 
-        public bool DeleteABook(int id)
+        //CRUD operations:
+
+        /// <summary>
+        /// Adding a new book object to the list
+        /// </summary>
+        /// <param name="newBook">Book item</param>
+        /// <returns>A book item</returns>
+        public Book AddNewBook(Book newBook)
         {
-            throw new NotImplementedException();
+            //Check if adding an object at all
+            if (newBook == null)
+            {
+                throw new ArgumentNullException(nameof(newBook));
+            }
+
+            //increment the ID by one
+            newBook.BookId = counter++;
+
+            //Add the item
+            books.Add(newBook);
+            return newBook;
+            
         }
 
+        
+        /// <summary>
+        /// Retrieving all books
+        /// </summary>
+        /// <returns>A list of books</returns>
         public List<Book> GetAllBooks()
         {
-            throw new NotImplementedException();
+            return books;
         }
 
-        public Book GetBookById(int id)
+        /// <summary>
+        /// Retrieve the book by its id
+        /// </summary>
+        /// <param name="bookId">BookID</param>
+        /// <returns>Matching/Corresponding book</returns>
+        public Book GetBookById(int bookId)
         {
-            throw new NotImplementedException();
+            return books.Find(b => b.BookId == bookId);
         }
 
-        public bool UpdateABook(Book item)
+
+        /// <summary>
+        /// Updating a book object
+        /// </summary>
+        /// <param name="updatedBook"></param>
+        /// <returns></returns>
+        public bool UpdateBook(Book updatedBook)
         {
-            throw new NotImplementedException();
+            //Checking if updating an object at all
+            if (updatedBook == null)
+            {
+                throw new ArgumentNullException(nameof(updatedBook));
+            }
+            //If exist find by its index
+            int idx = books.FindIndex(b => b.BookId == updatedBook.BookId);
+
+            //Check not to get negative index by deleting "too many"
+            if (idx == -1)
+            {
+                return false;
+            }
+
+            //Removing old index and update with new book
+            books.RemoveAt(idx);
+            books.Add(updatedBook);
+            return true;
+            
         }
+
+
+        /// <summary>
+        /// Deleting a book by its Id
+        /// </summary>
+        /// <param name="bookId">BookID</param>
+        /// <returns>True if the book exist</returns>
+        public bool DeleteBook(int bookId)
+        {
+            //Find book object by its index
+            int idx = books.FindIndex(b => b.BookId == bookId);
+
+            //Check not to get negative index by deleting "too many"
+            if (idx == -1)
+            {
+                return false;
+            }
+
+            //Remove all object with corresponding index from list
+            books.RemoveAll(b => b.BookId == bookId);
+            return true;
+        }
+
+       
     }
 }
